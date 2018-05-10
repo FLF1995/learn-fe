2984a2198c340eec77db40ae66e9dddd09f16ca2
FLF1995 <carolfan17@163.com>
2018-05-10 17:11:46 +0800

    demo2

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,10 @@
-console.info('zzzz')
+function test(num){
+	var times = 2;
+	var closure = function(){
+		var tt = 1;
+		return times * 2;
+	}
+	return closure();
+}
+console.log(test(10));
+console.log(times);
