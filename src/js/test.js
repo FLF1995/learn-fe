45e7e0037a6ed99192c9b0cb2c1922ce4e84e189
FLF1995 <carolfan17@163.com>
2018-06-04 09:13:30 +0800

    demo11 demo12

--- src/js/test.js
+++ src/js/test.js
@@ -1,10 +1,28 @@
-function test(num){
-	var times = 2;
-	var closure = function(){
-		var tt = 1;
-		return times * 2;
-	}
-	return closure();
+function test(num) {
+		var times = 2;
+		var closure = function () {
+				var tt = 1;
+				return times * 2;
+		}
+		return closure();
 }
 console.log(test(10));
 console.log(times);
+function doSomething() {
+		console.log(this);
+}
+element.onclick = function () {
+		console.log(this)
+}
+
+gsm = {
+	name: 'flf',
+	onclick(){
+		console.log(this)
+		doSomething()
+	}
+}
+
+element.onclick = function () {
+		doSomething()
+}
